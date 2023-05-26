dbb7541a5f8d4c319e4a3f1b863980e3fd2eae26
Tuuli Perttula <perttula@kat-laskenta.fmi.fi>
2023-05-26 11:00:00 +0000

    Taking into account error propagation etc

--- tropomi_l2_to_l3.py
+++ tropomi_l2_to_l3.py
@@ -35,13 +35,14 @@ def get_bin_spatial_string(conf):
     return bin_spatial_string
 
 
-def merge_and_regrid(conf, infiles, timeperiod):
+def merge_and_regrid(conf, infiles, timeperiod, variable):
     """ Regrid and merge multiple l2 input files into one l3 output file.
 
     Keyword arguments:
     conf -- config dictionary
     infiles -- input filenames, contains star as a wildcard character
     timeperiod -- timeperiod to merge, day|month
+    variable -- variable to merge
 
     Return:
     merged -- regridded and merged data
@@ -51,22 +52,41 @@ def merge_and_regrid(conf, infiles, timeperiod):
     first_input_file = glob.glob(infiles)[0]
 
     data = harp.import_product(first_input_file)
-    
-    if f'{conf["variable"]["harp_var_name"]}_validity' in data:
-        logger.debug(f'Using {conf["variable"]["harp_var_name"]}_validity in merging files.')
-        operations = ";".join([
-            f'{conf["variable"]["harp_var_name"]}_validity>{conf["variable"]["validity_min"]}',
-            f'keep(latitude_bounds,longitude_bounds,datetime_start,datetime_length,{conf["variable"]["harp_var_name"]})',
-            f'derive({conf["variable"]["harp_var_name"]} float [{conf["variable"]["unit"]}])',
-            "derive(datetime_stop {time} [days since 2000-01-01])",
-            "derive(datetime_start [days since 2000-01-01])",
-            "exclude(datetime_length)",
-            bin_spatial_string,
-            "derive(latitude float {latitude})",
-            "derive(longitude float {longitude})"
-        ])
+
+    if timeperiod == "day":
         
-        reduce_operations = f'squash(time, (latitude, longitude, latitude_bounds, longitude_bounds));bin()'
+        logger.debug('Merging one day of data.')
+
+        if "no2" in variable or "co-" in variable:
+            operations = ";".join([
+                f'{conf["variable"]["harp_var_name"]}_validity>{conf["variable"]["validity_min"]}',
+                "derive(solar_hour_angle {time})", f"solar_hour_angle>{22.5-65}", f"solar_hour_angle<{22.5+65}",
+                f'keep(latitude_bounds,longitude_bounds,datetime_start,datetime_length,{conf["variable"]["harp_var_name"]},{conf["variable"]["harp_var_name"]}_uncertainty)',
+                f'derive({conf["variable"]["harp_var_name"]} float [{conf["variable"]["unit"]}])',
+                f'derive({conf["variable"]["harp_var_name"]}_uncertainty float [{conf["variable"]["unit"]}])',
+                "derive(datetime_stop {time} [days since 2000-01-01])",
+                "derive(datetime_start [days since 2000-01-01])",
+                "exclude(datetime_length)",
+                bin_spatial_string,
+                "derive(latitude float {latitude})",
+                "derive(longitude float {longitude})"
+            ])
+
+        else:            
+            operations = ";".join([
+                f'{conf["variable"]["harp_var_name"]}_validity>{conf["variable"]["validity_min"]}',
+                f'keep(latitude_bounds,longitude_bounds,datetime_start,datetime_length,{conf["variable"]["harp_var_name"]},{conf["variable"]["harp_var_name"]}_uncertainty)',
+                f'derive({conf["variable"]["harp_var_name"]} float [{conf["variable"]["unit"]}])',
+                f'derive({conf["variable"]["harp_var_name"]}_uncertainty float [{conf["variable"]["unit"]}])',
+                "derive(datetime_stop {time} [days since 2000-01-01])",
+                "derive(datetime_start [days since 2000-01-01])",
+                "exclude(datetime_length)",
+                bin_spatial_string,
+                "derive(latitude float {latitude})",
+                "derive(longitude float {longitude})"
+            ])
+
+        reduce_operations = 'squash(time, (latitude, longitude, latitude_bounds, longitude_bounds));set("propagate_uncertainty", "uncorrelated");bin()'
         post_operations = "exclude(weight, longitude_bounds, latitude_bounds)"
 
         logger.debug(f'Merging files {infiles} into one')
@@ -78,36 +98,47 @@ def merge_and_regrid(conf, infiles, timeperiod):
         
     elif timeperiod == "month":
         logger.debug(f'Merging one month of data and using already merged l3 data as input.')
-        """ operations = ";".join([
-            f'keep(latitude, longitude, datetime_start,datetime_stop,{conf["variable"]["harp_var_name"]})',
-            f'derive({conf["variable"]["harp_var_name"]} float [{conf["variable"]["unit"]}])',
-            "derive(datetime_stop {time} [days since 2000-01-01])",
-            "derive(datetime_start [days since 2000-01-01])",
-        ])
-        reduce_operations = "squash(time, (latitude, longitude));bin()"
-        post_operations = "exclude(weight)" """
         
         post_operations = "squash(time, (latitude, longitude));bin()"
-
+        
         logger.debug(f'Merging files {infiles} into one')
         try:
-            #merged = harp.import_product(infiles, operations, reduce_operations = reduce_operations, post_operations = post_operations)
             merged = harp.import_product(infiles, post_operations = post_operations)
         except Exception as e:
             logger.error(f'Error while merging files with HARP')
             logger.error(e)
+            
     else:
-        raise ValueError("Validity parameter not found in input files while trying to merge one day of data. Aborting.")
+        raise ValueError(f"Given timeperiod {timeperiod} is not valid.")
         
     return merged
 
 
-def edit_netcdf_file(netcdf_file):
+def read_attributes_from_file(netcdf_file):
+    """ Read extra attributes (not supported by HARP) 
+    from first input file.
+    
+    Keyword arguments:
+    netcdf_file -- input file
+
+    Return:
+    processor_version -- processor_version attribute
+
+    """
+    
+    with nc.Dataset(netcdf_file, 'r', format='NETCDF4') as nc_file:        
+        processor_version = nc_file.getncattr('processor_version')
+
+    return processor_version
+
+
+def edit_netcdf_file(netcdf_file, processor_version):
     """ Add extra attributes to file using netCDF4 library. 
     Has to be done separately because HARP does not support this.
 
     Keyword arguments:
     netcdf_file -- merged output file
+    processor_version -- processor_version attribute
 
     Return:
     nc_file -- output file with added attributes
@@ -120,6 +151,7 @@ def edit_netcdf_file(netcdf_file):
             nc_file.data_provider = "Finnish Meteorological Institute"
             nc_file.data_origin = "Copernicus Sentinel-5P/TROPOMI"
             nc_file.legal_notice = "Contains modified Copernicus Sentinel data processed by Finnish Meteorological Institute."
+            nc_file.processor_version = processor_version
             
     except Exception as e:
         logger.error(f'Error while writing attribute to file {netcdf_file}')
@@ -143,14 +175,18 @@ def main():
     # Merge and re-grid files
     infiles = f'{conf["input"][options.timeperiod]["path"]}/{conf["input"][options.timeperiod]["filename"].format(date=options.date)}'
     outfile = f'{conf["output"][options.timeperiod]["path"]}/{conf["output"][options.timeperiod]["filename"].format(date=options.date)}'
-    merged = merge_and_regrid(conf, infiles, options.timeperiod)
+    merged = merge_and_regrid(conf, infiles, options.timeperiod, options.var)
     
     # Write merged data to l3 output file
     logger.debug(f'Writing merged product to file {outfile}')
     harp.export_product(merged, outfile)
 
+    # Read attributes from netcdf file using netCDF4 reader
+    first_input_file = glob.glob(infiles)[0]
+    processor_version = read_attributes_from_file(first_input_file)
+    
     # Add extra attributes to output file
-    outfile = edit_netcdf_file(outfile)
+    outfile = edit_netcdf_file(outfile, processor_version)
 
     
 if __name__ == '__main__':
